80cd47a755016c0ca2e6b3f5300bd98fb56424ec
DrinePsylook <nmohaouzine.sandrine@gmail.com>
2025-01-08 19:51:54 +0100

    transfo txt en xml

--- app.py
+++ app.py
@@ -5,7 +5,7 @@ import os
 import io
 import tempfile
 
-from utils import transform_xml, clean_dies
+from utils_txt import transform_xml, clean_dies, create_custom_xml
 
 st.set_page_config(page_title="ROM video games",
                    page_icon="🎮",
@@ -30,40 +30,32 @@ with col2:
     
     
     if uploaded_txt is not None :
-        # st.write(uploaded_txt.getvalue())
-        # st.write(message_loading)
-        file_uploaded = clean_dies(uploaded_txt.getvalue())
-        # print("file_uploaded = " , file_uploaded[:10])
-    
-        # transforme le fichier uploadé en string
-        # file_uploaded = io.StringIO(uploaded_txt.getvalue().decode('utf-8'))
+        with st.spinner('Chargement du fichier en cours...'):
+            # st.write(uploaded_txt.getvalue())
+            file_uploaded = clean_dies(uploaded_txt.getvalue())
 
-        # transformation du fichier txt uploadé
-        df_cleaned = transform_xml(file_uploaded)
-        st.write("Aperçu des données nettoyées :")
-        st.write(df_cleaned.head())
-        # récupération du nom initial du txt pour renommer le nouveau fichier downloadé
-        name0 = uploaded_txt.name.split(".")
-        new_df_name = f"New_{name0[0]}.xml".replace(" ", "_")
+            # transformation du fichier txt uploadé
+            df_cleaned = transform_xml(file_uploaded)
+            xml_content = create_custom_xml(df_cleaned)
+            
+            # récupération du nom initial du txt pour renommer le nouveau fichier downloadé
+            name0 = uploaded_txt.name.split(".")
+            new_df_name = f"New_{name0[0]}.xml".replace(" ", "_")
 
-        # utilisation du temp pour télécharger le fichier dans le dossier téléchargement
-        temp_dir = tempfile.mkdtemp()
-        path = os.path.join(temp_dir, new_df_name)
-        df_cleaned.to_xml(path, index=False, parser='etree')
-        
-        with open(path, 'r', encoding='utf-8') as file: 
-            xml_content = file.read()
+            # utilisation du temp pour télécharger le fichier dans le dossier téléchargement
+            temp_dir = tempfile.mkdtemp()
+            path = os.path.join(temp_dir, new_df_name)
 
-        # affichage du bouton de téléchargement du fichier
-        valid_txt= st.download_button(
-            label="Télécharger au format xml",
-            data=xml_content,
-            file_name = new_df_name,
-            mime="application/xml"
-            )
-        
-        if valid_txt:
-            message_loading = ""
 
-        
+            # affichage du bouton de téléchargement du fichier
+            valid_txt= st.download_button(
+                label="Télécharger au format xml",
+                data=xml_content,
+                file_name = new_df_name,
+                mime="application/xml"
+                )
+            
+            if valid_txt: 
+                st.rerun()
 
+       
---
+++ utils_txt.py
@@ -0,0 +1,81 @@
+import streamlit as st
+import pandas as pd
+import numpy as np
+import xml.etree.ElementTree as ET
+from xml.dom import minidom
+import re
+import os
+import io
+
+def clean_dies(file_verif): 
+    # Conversion des bytes en string
+    file_verif_str = file_verif.decode('utf-8')
+    lines = file_verif_str.splitlines()
+    cleaned_lines = [lines[0]] + [line for line in lines[1:] if not line.startswith("#")]
+    return cleaned_lines
+
+def clean_xml_string(text):
+    if pd.isna(text):
+        return ""
+    
+    text = str(text)
+    if text == "":
+        return None
+
+    text = text.replace('&', '&amp;')
+    text = text.replace('<', '&lt;')
+    text = text.replace('>', '&gt;')
+    text = text.replace('"', '&quot;')
+    text = text.replace("'", '&apos;')
+    
+    return text
+
+def adjust_columns(header, lines): 
+    # fonction qui ajuste et redimensionne le tableau s'il y a une différence entre la taille des colonnes et des données par ligne -> malheureusement, perte de données
+    adjusted_data = [] 
+    for line in lines: 
+        columns = line.strip().split(';') 
+        if len(columns) > len(header): 
+            diff = len(columns) - len(header)
+            columns = columns[:-diff] 
+            # Retire la/les dernière(s) colonne en trop 
+        adjusted_data.append(columns) 
+    return adjusted_data
+
+def transform_xml(cleaned_lines):
+    header = cleaned_lines[0].replace("#", "").strip().split(';')
+    data_lines = cleaned_lines[1:]
+    data = [line.split(";") for line in data_lines]
+    if len(data_lines) > len(header):
+        adjusted_data = adjust_columns(header, data_lines) 
+
+    df_file = pd.DataFrame(adjusted_data, columns=header)
+    df_file = df_file.applymap(lambda x: np.nan if x is None else x)
+    for col in df_file.columns:
+        df_file[col] = df_file[col].astype(object).replace("", np.nan)
+
+    file_xml = df_file.reset_index(drop=True)
+    file_xml.columns = file_xml.columns.str.lower()
+    print("file xml colonnes = ",file_xml.columns)
+    
+    for col in df_file.columns :
+        df_file[col] = df_file[col].apply(clean_xml_string)
+
+    return file_xml
+
+def create_custom_xml(df):
+    xml_str = '<?xml version="1.0" encoding="UTF-8"?>\n'
+    xml_str += '<data>\n'
+
+    for _, row in df.iterrows():
+        xml_str += '    <row>\n'
+        for col in df.columns:
+            value = clean_xml_string(row[col])
+            if value is not None: 
+                xml_str += f'   <{col}>{value}</{col}>\n'
+                # print("valeur = ",value)
+            else:
+                xml_str +=f'   <{col}>{np.nan}</{col}>\n'
+        xml_str += '    </row>\n'
+    xml_str += '</data>'
+    return xml_str
